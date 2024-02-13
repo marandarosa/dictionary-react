710e4600d4e77462ae79d72888e428259281647e
marandarosa <marandavalentin@aol.com>
2024-02-12 19:56:25 -0500

    Edit header styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <header className="text-center">
-        <h1 className="mt-5">Dictionary</h1>
+        <h1 className="mt-5">
+          <strong>Dictionary</strong>
+        </h1>
       </header>
     </div>
   );
