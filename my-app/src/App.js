2aaec8400876a9ca080424a48b09b1cf7cfecff2
marandarosa <marandavalentin@aol.com>
2024-02-12 19:49:22 -0500

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="text-center">
+        <h1 className="mt-5">Dictionary</h1>
+      </header>
+    </div>
+  );
+}
